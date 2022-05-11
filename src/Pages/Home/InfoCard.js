4f0971790192224aebb3f258fba1470f2902d388
emonmorol <ekramulhasane69@gmail.com>
2022-05-11 13:47:49 +0600

    info cornered border

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -4,8 +4,14 @@ const InfoCard = ({ info }) => {
   const { title, context, image, infoBg } = info;
   return (
     <div
-      className={`card items-center lg:card-side shadow-xl ${infoBg} text-white py-10`}
+      className={` relative card rounded-none rounded-tl-2xl rounded-br-2xl items-center overflow-visible lg:card-side shadow-xl ${infoBg} text-white py-10`}
     >
+      <div className="absolute bottom-0 left-0 w-24 h-24 border-b-4 border-l-4 border-orange-300">
+        {" "}
+      </div>
+      <div className="absolute top-0 right-0 w-24 h-24 border-t-4 border-r-4 border-orange-300">
+        {" "}
+      </div>
       <figure className="lg:pl-10">
         <img src={image} alt="Album" />
       </figure>
